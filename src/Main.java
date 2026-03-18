e5c5fe173c6e52433ef2c261fdc8b2722253f236
arabdrakh <nurasyloran@gmail.com>
2026-03-18 12:36:12 +0500

    add all tasks

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        System.out.println("Task 1:");
+        part1.printDigits(5481);
+
+
+        System.out.println("\nTask 2:");
+        int[] arr = {3, 2, 4, 1};
+        int sum = part1.sum(arr, arr.length);
+        System.out.println("Average: " + (double) sum / arr.length);
+
+        System.out.println("\nTask 3:");
+        System.out.println(part1.isPrime(7, 2) ? "Prime" : "Composite");
+
+        System.out.println("\nTask 4:");
+        System.out.println(part1.factorial(5));
+    }
 }
--- src/part1.java
+++ src/part1.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
-
 public class part1 {
+
+    // Abdrakhmanova Aruzhan IT-2501
+    public static void printDigits(int n) {
+        if (n < 10) {
+            System.out.println(n);
+            return;
+        }
+        printDigits(n / 10);
+        System.out.println(n % 10);
+    }
+
+    public static int sum(int[] arr, int n) {
+        if (n == 0) return 0;
+        return arr[n - 1] + sum(arr, n - 1);
+    }
+    public static boolean isPrime(int n, int i) {
+        if (n <= 2) return (n == 2);
+        if (n % i == 0) return false;
+        if (i * i > n) return true;
+        return isPrime(n, i + 1);
+    }
+
+    public static int factorial(int n) {
+        if (n == 0) return 1;
+        return n * factorial(n - 1);
+    }
 }
